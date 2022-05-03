a7160fad1744452398d308702087b13b875c7978
Edgar Benavides <edgarben27@gmail.com>
2022-05-03 16:28:19 -0500

    hook usePhotoGallery implemented

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,11 +1,11 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import { camera } from 'ionicons/icons';
 import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
-  const takePhoto = () => {
-    alert('TAKE PHOTO')
-  }
+  const {photos, takePhoto} = usePhotoGallery()
+  console.log(photos)
   return (
     <IonPage>
       <IonHeader>
@@ -14,6 +14,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical='bottom' horizontal='center' slot='fixed'>
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}/>
